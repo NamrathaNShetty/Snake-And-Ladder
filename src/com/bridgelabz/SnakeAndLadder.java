b72c65c9cf2dced021d0106ed43f18f180a47d91
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-05 19:28:34 +0530

    Added: Initializing the position

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
-        int position = 0;
+        int position = 0;     //initializing start position to 0
     public static void main(String[] args) {
         System.out.println("Snake and Ladder problem");
     }
